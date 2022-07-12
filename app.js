ff778aea891ac0c800faba6535bf76fa2c304125
Anthony Gomez <gomez9162@gmail.com>
2022-07-11 22:31:48 -0500

    testing

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const PORT = 3000
 
 // Static Files
 app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/img', express.static(__dirname + 'public/img'))
-app.use('/js', express.static(__dirname + 'public/js'))
+
 
 // Templating Engine
 app.set('views', './src/views')
