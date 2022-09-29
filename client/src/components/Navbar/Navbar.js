0b7dd0b4a9a8b136254796807ff10fbda1b417b4
Anthony Gomez <gomez9162@gmail.com>
2022-09-28 23:44:15 -0500

    Work on responsive navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,26 +1,34 @@
-import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
-import cart from '../../assets/icon-cart.svg'
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import cart from "../../assets/icon-cart.svg";
 
 const Navbar = () => {
   return (
     <div>
-      <nav>
-        <div className='flex flex-col'>
-          <div>
-          <Link to='/'>
-            <img src={logo} alt='Audiophile Logo'/>
-          </Link>
+      <nav className="bg-p-color">
+        <div className=" container mx-auto w-4/5 h-24 flex flex-row justify-between items-center ">
+          <div className="">
+            <Link to="/">
+              <img src={logo} alt="Audiophile Logo" />
+            </Link>
           </div>
-          <ul className='flex flex-row'>
-            <li><Link to='/home'>Home</Link></li>
-            <li><Link to='/headphones'>Headphones</Link></li>
-            <li><Link to='speakers'>Speakers</Link></li>
-            <li><Link to='earphones'>Earphones</Link></li>
+          <ul className="flex space-x-9 text-w-color uppercase font-Manrope text-sm font-medium hidden">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/headphones">Headphones</Link>
+            </li>
+            <li>
+              <Link to="speakers">Speakers</Link>
+            </li>
+            <li>
+              <Link to="earphones">Earphones</Link>
+            </li>
           </ul>
           <div>
-            <Link to='/cart'>
-              <img src={cart} alt='Cart Icon'/>
+            <Link to="/cart">
+              <img src={cart} alt="Cart Icon" />
             </Link>
           </div>
         </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'p-color': '#101010',
+        'o-color': '#D87D4A',
+        'g-color': '#F1F1F1',
+        'w-color': '#FAFAFA'
+      },
+      fontFamily: {
+        Manrope: ['Manrope', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
